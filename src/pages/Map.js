73e7ecc4bdbc2483bd3b88020b2fae3a881d3792
daisuke.miyoshi <nana40481023@gmail.com>
2022-12-06 21:20:24 +0900

    Add　全店舗をmapに表示

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { GoogleMap, LoadScript, MarkerF, InfoWindow} from "@react-google-maps/api";
+import axios from "axios";
 
 export default function Map() {
+  const [allCordinate, setAllCordinate] = useState([]);
+  useEffect(() => {
+    axios
+      .get(process.env.NEXT_PUBLIC_BACKEND_API_URL + "shop")
+      .then((res) => {
+        const allCordinateInfo= [];
+        res.data.forEach((shopInfo) => {
+          allCordinateInfo.push({lat: shopInfo.Lat, lng: shopInfo.Lng})
+        })
+        setAllCordinate(allCordinateInfo)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  },[])
   const router = useRouter();
-
   const containerStyle = {
     height: "100vh",
     width: "100%",
@@ -41,8 +56,11 @@ export default function Map() {
 
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY} onLoad={() => createOffsetSize()}>
-    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
-        <MarkerF position={positionAkiba} />
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}> 
+      {allCordinate.map((cordinate, index) => {
+        return <MarkerF position={cordinate} key={index} />
+      })}
+        {/* <MarkerF position={positionAkiba} />
         <MarkerF position={positionIwamotocho} />
         <InfoWindow position={positionAkiba} options={infoWindowOptions}>
           <div style={divStyle}>
@@ -53,7 +71,7 @@ export default function Map() {
           <div style={divStyle}>
             <h1>岩本町オフィス</h1>
           </div>
-        </InfoWindow>
+        </InfoWindow> */}
       </GoogleMap>
     </LoadScript>
   );
