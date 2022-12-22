f4536e41ad4012eb16d6ad07be090d095853160b
daisuke.miyoshi <nana40481023@gmail.com>
2022-12-22 12:11:57 +0900

    Add ハンバーガーメニューを押したら drawerが出てくるようにした

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import Drawer from '@mui/material/Drawer';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
+import LockIcon from '@mui/icons-material/Lock';
+import LogoutIcon from '@mui/icons-material/Logout';
 import styles from "../styles/Header.module.css"
 
 export default function Header() {
@@ -11,8 +15,25 @@ export default function Header() {
       <span className={styles.menu} onClick={() => setDrawerOpened(true)}>
         <hr /><hr /><hr />
       </span>
-      <Drawer anchor="right" open={drawerOpened} onClose={() => setDrawerOpened(false)}>
-        <p>hello</p>
+      <Drawer className={styles.drawer} anchor="right" open={drawerOpened} onClose={() => setDrawerOpened(false)}>
+        <ul className={styles["bullet-point"]}>
+          <li>
+            <HomeIcon className={styles.icon} />
+            TOPページ
+          </li>
+          <li>
+            <AccountCircleIcon className={styles.icon} />
+            ユーザー名変更
+          </li>
+          <li>
+            <LockIcon className={styles.icon} />
+            パスワード変更
+          </li>
+          <li>
+            <LogoutIcon className={styles.icon} />
+            ログアウト
+          </li>
+        </ul>
       </Drawer>
     </header>
   )
