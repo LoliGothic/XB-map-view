0c2ef48de7c7c89d3e0093e4e96010804efde1b7
daisuke.miyoshi <nana40481023@gmail.com>
2022-12-23 18:24:11 +0900

    Add 箇条書きをクリックするとそれぞれのページに遷移するようにした

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRouter } from 'next/router';
 import Drawer from '@mui/material/Drawer';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HomeIcon from '@mui/icons-material/Home';
@@ -8,30 +9,50 @@ import styles from "../styles/Header.module.css"
 
 export default function Header() {
   const [drawerOpened, setDrawerOpened] = useState(false);
+  const router = useRouter();
+
+  async function transitionHome() {
+    await router.push({pathname: "/"});
+
+    // そのままhomeに戻るとapi alredy presentedエラーが出るのでリロードしてあげる
+    location.reload();
+  }
+
+  function transitionChangeName() {
+    router.push({pathname: "/ChangeName"});
+  }
+
+  function transitionChangePassword() {
+    router.push({pathname: "/ChangePassword"});
+  }
+
+  function logout() {
+    router.push({pathname: "Login"});
+  }
 
   return (
     <header className={styles.header}>
-      <img src="/text_header-title.png" alt="title" />
+      <img src="/text_header-title.png" alt="title" onClick={transitionHome} />
       <span className={styles.menu} onClick={() => setDrawerOpened(true)}>
         <hr /><hr /><hr />
       </span>
       <Drawer className={styles.drawer} anchor="right" open={drawerOpened} onClose={() => setDrawerOpened(false)}>
         <ul className={styles["bullet-point"]}>
-          <li>
+          <li onClick={transitionHome}>
             <HomeIcon className={styles.icon} />
-            TOPページ
+            <p>TOPページ</p>
           </li>
-          <li>
+          <li onClick={transitionChangeName}>
             <AccountCircleIcon className={styles.icon} />
-            ユーザー名変更
+            <p>ユーザー名変更</p>
           </li>
-          <li>
+          <li onClick={transitionChangePassword}>
             <LockIcon className={styles.icon} />
-            パスワード変更
+            <p>パスワード変更</p>
           </li>
-          <li>
+          <li onClick={logout}>
             <LogoutIcon className={styles.icon} />
-            ログアウト
+            <p className={styles.logout}>ログアウト</p>
           </li>
         </ul>
       </Drawer>
