e8bbca7d5aaf5a102413fe17835a0b41b9896ca7
daisuke.miyoshi <nana40481023@gmail.com>
2023-01-04 20:24:37 +0900

    Fix 環境変数がうまくできているか

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,7 +17,6 @@ export default function Login() {
   // ログイン画面に遷移した時はlocalStorageをすべて削除する
   useEffect(() => {
     localStorage.clear();
-    console.log(process.env.NEXT_PUBLIC_BACKEND_API_URL);
   },[])
 
   function switching() {
@@ -39,7 +38,7 @@ export default function Login() {
 
     // json形式でバックエンドにname,password,emailをpostする
     axios
-      .post("xb-map-back.azurewebsites.net/" + "signup", {
+      .post(process.env.NEXT_PUBLIC_BACKEND_API_URL + "signup", {
         name: name.current.value,
         password: signupPassword.current.value,
         checkPassword: checkSignupPassword.current.value,
